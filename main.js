87f579b385b93c7e90dd9270680400cdcc413548
Szymon <szym0nsuchy.06@gmail.com>
2022-06-15 20:42:34 +0200

    HBS setup

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-    res.send('No Bitches?');
+    res.render('index');
 });
 
 app.listen(80);
